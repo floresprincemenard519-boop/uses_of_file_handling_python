763090e8c048424ba0e6b9518a77c7195bb0671e
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-26 01:45:27 +0800

    The method workedd but, I observed that the txt files are in the main repo folder which should be inside their own folder which is the how_pythons_read. A solution i can think of is to open the folder im currently working on instead of open the repo folder in my IDE.

--- how_pythons_read/a_python_poet_that_can_read_and_write.py
+++ how_pythons_read/a_python_poet_that_can_read_and_write.py
@@ -3,6 +3,11 @@ import random
 with open("numbers.txt", "w") as numbers:
     for counts in range(20):
         numbers.write(str(random.randint(-100, 100)) + "\n")
+with open("even.txt", "w") as even:
+    even.write("")
+
+with open("odd.txt", "w") as odd:
+    odd.write("")
 
 with open("numbers.txt", "r") as numbers:
     for line in numbers:
