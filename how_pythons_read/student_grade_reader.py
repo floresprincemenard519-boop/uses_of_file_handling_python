5617791eb17900ce0a8c6896fff267601be49951
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-26 20:24:50 +0800

    Created the main logic for comparison. Next step is to print the top studnet.

--- how_pythons_read/student_grade_reader.py
+++ how_pythons_read/student_grade_reader.py
@@ -1,4 +1,12 @@
+highest_grade = 0
+top_student = ""
+
 with open("student_grades.txt", "r") as file:
     for line in file:
         student_name, grade = line.strip().split(",")
+        grade = float(grade)
         print(f"Student: {student_name}, Grade: {grade}")
+        if grade > highest_grade:
+            highest_grade = grade
+            top_student = student_name
+
