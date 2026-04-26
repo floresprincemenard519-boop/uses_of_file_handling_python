2718ef1a89cec4ce15bb1b8bad50e91ad52b6942
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-26 20:35:00 +0800

    Changed variables cause the logic is wrong. 1 is the highest grade and 5 is the lowest. The code is now done will comeback later for more additions.

--- how_pythons_read/student_grade_reader.py
+++ how_pythons_read/student_grade_reader.py
@@ -1,13 +1,12 @@
-highest_grade = 0
+highest_grade = 5
 top_student = ""
 
 with open("student_grades.txt", "r") as file:
     for line in file:
         student_name, grade = line.strip().split(",")
         grade = float(grade)
-        print(f"Student: {student_name}, Grade: {grade}")
-        if grade > highest_grade:
+        if grade < highest_grade:
             highest_grade = grade
             top_student = student_name
 
-print(f"\nTop Student: {top_student} with a grade of {highest_grade}")
+print(f"Top Student: {top_student} with a grade of {highest_grade}")
