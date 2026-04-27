0abbf3203820cfc1c64ad33478aeac04316a1826
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-27 09:05:03 +0800

    Added 4 line of code so that every time the code is ran the current files for triple and double are deleted.

--- how_pythons_use_methods_to_read_and_write/square_and_cude_generator.py
+++ how_pythons_use_methods_to_read_and_write/square_and_cude_generator.py
@@ -1,4 +1,8 @@
 with open("integers.txt", "r") as file:
+    with open("double.txt", "w") as squared_number:
+        pass
+    with open("triple.txt", "w") as cubed_number:
+        pass
     for line in file:
         number = int(line.strip())
         if number % 2 == 0:
