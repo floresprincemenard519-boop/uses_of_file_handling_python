f0edfdd4c55bebb44d126bdca16aa6301b9eb032
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-27 10:19:48 +0800

    Added error handling for value error and file not found. Also added a checker for format. It wil skip a line if the format is incorrect.

--- how_pythons_read/student_grade_reader.py
+++ how_pythons_read/student_grade_reader.py
@@ -4,7 +4,11 @@ top_student = "No student in file."
 try:
     with open("student_grades.txt", "r") as file:
         for line in file:
-            student_name, grade = line.strip().split(",")
+            format_checker = line.strip().split(",")
+            if len(format_checker) != 2:
+                print(f"Incorrect format: {line.strip()}. Expected 'Name,Grade'")
+                continue
+            student_name, grade = format_checker 
             try:
                 grade = float(grade)
             except ValueError:
@@ -15,5 +19,7 @@ try:
                 top_student = student_name
 except FileNotFoundError:
     print("Error: File not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 print(f"Top Student: {top_student} with a grade of {highest_grade}")
