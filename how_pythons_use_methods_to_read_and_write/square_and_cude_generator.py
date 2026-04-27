9be0b5935213f3033f570b330f3bf711362fc41d
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-27 11:25:24 +0800

    added error handling for value error and if the file is not found/ if the file have a space or empty line it will skip it but if it has a number it will raise an valueerror and skip it.

--- how_pythons_use_methods_to_read_and_write/square_and_cude_generator.py
+++ how_pythons_use_methods_to_read_and_write/square_and_cude_generator.py
@@ -1,12 +1,30 @@
-def square_and_cube_generator(file_name):
-    with open(file_name, "r") as file, \
-         open("double.txt", "w") as squared_number, \
-         open("triple.txt", "w") as cubed_number:
-        for line in file:
-            number = int(line.strip())
-            if number % 2 == 0:
-                squared_number.write(f"{number ** 2}\n")
-            elif number % 2 == 1:
-                cubed_number.write(f"{number ** 3}\n")
+class SquareAndCubeGenerator:
+    def __init__(self, file_name):
+        self.file_name = file_name
 
-square_and_cube_generator("integers.txt")
+    def square_and_cube_generator(self):
+        try:
+            with open(self.file_name, "r") as file, \
+                open("double.txt", "w") as squared_number, \
+                open("triple.txt", "w") as cubed_number:
+
+                for line in file:
+                    number = line.strip()
+                    if not number:
+                        continue
+                    try:
+                        number = int(number)
+                        if number % 2 == 0:
+                            squared_number.write(f"{number ** 2}\n")
+                        elif number % 2 == 1:
+                            cubed_number.write(f"{number ** 3}\n")
+                    except ValueError:
+                        print(f"Invalid number: {line.strip()}")
+
+        except FileNotFoundError:
+            print(f"File not found: {self.file_name}")
+        except Exception as error:
+            print(f"An error occurred: {error}")
+
+generator = SquareAndCubeGenerator("integers.txt")
+generator.square_and_cube_generator()
--- how_pythons_write_with_method/writing_in_text_file_using_a_method.py
+++ how_pythons_write_with_method/writing_in_text_file_using_a_method.py
@@ -3,11 +3,11 @@ class Writer:
         self.file_name = file_name
 
     def write_to_file(self):
-        while True:
-            text_to_input = input("Enter what you want to write (Enter 'quit' to exit): ")
-            if text_to_input == "quit":
-                break
-            with open(self.file_name, "a") as file:
+        with open(self.file_name, "a") as file:
+            while True:
+                text_to_input = input("Enter what you want to write (Enter 'quit' to exit): ")
+                if text_to_input == "quit":
+                    break
                 file.write(text_to_input + "\n")
             
 writer = Writer("mylife.txt")
