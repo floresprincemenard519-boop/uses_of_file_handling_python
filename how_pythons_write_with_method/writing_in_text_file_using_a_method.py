c48e9aebc42ef9ad6259551fbb18a8687e009e15
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-27 09:35:30 +0800

    removed a parameter on the function so that i can make a loop inside the function. I also created the main logic and will clean this up later.

--- how_pythons_write_with_method/writing_in_text_file_using_a_method.py
+++ how_pythons_write_with_method/writing_in_text_file_using_a_method.py
@@ -1,10 +1,12 @@
-while True:
-    text_to_input = input("Enter what you want to write (Enter 'quit' to exit): ")
-    if text_to_input == "quit":
-        break
+def write_to_file(file_name):
+    while True:
+        text_to_input = input("Enter what you want to write (Enter 'quit' to exit): ")
+        if text_to_input == "quit":
+            break
+        with open(file_name, "a") as file:
+            file.write(text_to_input + "\n")
+        
+               
+    
 
-def write_to_file(file_name, text):
-    with open(file_name, "a") as file:
-        file.write(text)
-
-write_to_file("mylife.txt", text_to_input)
+write_to_file("mylife.txt")
