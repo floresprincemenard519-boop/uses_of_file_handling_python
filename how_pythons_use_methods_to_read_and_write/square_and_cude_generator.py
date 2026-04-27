0620531e3351a65f0a30661b537c1dff1f1ed7d1
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-27 08:57:28 +0800

    Added logic for detectin odd numbers and stored at another file.

--- how_pythons_use_methods_to_read_and_write/square_and_cude_generator.py
+++ how_pythons_use_methods_to_read_and_write/square_and_cude_generator.py
@@ -1,3 +1,9 @@
 with open("integers.txt", "r") as file:
     for line in file:
-        print(line)
+        number = int(line.strip())
+        if number % 2 == 0:
+            with open("double.txt", "a") as squared_number:
+                squared_number.write(f"{number ** 2}\n")
+        elif number % 2 == 1:
+            with open("triple.txt", "a") as cubed_number:
+                cubed_number.write(f"{number ** 3}\n")
