75ecdb7132f68ace67996ca0855e03aa89e740a5
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-27 09:14:43 +0800

    Edited the file so that it becomes shorter. Removed repeated code and some more polishing.

--- how_pythons_use_methods_to_read_and_write/square_and_cude_generator.py
+++ how_pythons_use_methods_to_read_and_write/square_and_cude_generator.py
@@ -1,13 +1,12 @@
-with open("integers.txt", "r") as file:
-    with open("double.txt", "w") as squared_number:
-        pass
-    with open("triple.txt", "w") as cubed_number:
-        pass
-    for line in file:
-        number = int(line.strip())
-        if number % 2 == 0:
-            with open("double.txt", "a") as squared_number:
+def square_and_cube_generator(file_name):
+    with open(file_name, "r") as file, \
+         open("double.txt", "w") as squared_number, \
+         open("triple.txt", "w") as cubed_number:
+        for line in file:
+            number = int(line.strip())
+            if number % 2 == 0:
                 squared_number.write(f"{number ** 2}\n")
-        elif number % 2 == 1:
-            with open("triple.txt", "a") as cubed_number:
+            elif number % 2 == 1:
                 cubed_number.write(f"{number ** 3}\n")
+
+square_and_cube_generator("integers.txt")
