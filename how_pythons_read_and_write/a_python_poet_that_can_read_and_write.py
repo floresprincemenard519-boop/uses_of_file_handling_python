522d09197c1219f87ee0f6e5fe76a52460ba0492
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-27 10:47:35 +0800

    removed the file opening inside the loop which is my mistake. more efficient to open once than to open each time in the loop. cleaned the code. also no errors cause the system resets every time the program is run. this is because it is a program not a function or a method. which is what is inside the problem question 'program' not 'method'

--- how_pythons_read_and_write/a_python_poet_that_can_read_and_write.py
+++ how_pythons_read_and_write/a_python_poet_that_can_read_and_write.py
@@ -3,20 +3,14 @@ import random
 with open("numbers.txt", "w") as numbers:
     for counts in range(20):
         numbers.write(str(random.randint(-100, 100)) + "\n")
-with open("even.txt", "w") as even:
-    even.write("")
 
-with open("odd.txt", "w") as odd:
-    odd.write("")
+with open("numbers.txt", "r") as numbers, \
+    open("even.txt", "w") as even, \
+    open("odd.txt", "w") as odd:
 
-with open("numbers.txt", "r") as numbers:
     for line in numbers:
         number = int(line.strip())
         if number % 2 == 0:
-            #The number is even..
-            with open("even.txt", "a") as even:
-                even.write(str(number) + "\n")  
-        elif number % 2 == 1:
-            #The number is odd..
-            with open("odd.txt", "a") as odd:
-                odd.write(str(number) + "\n")    
+            even.write(str(number) + "\n")  
+        else:
+            odd.write(str(number) + "\n")    
