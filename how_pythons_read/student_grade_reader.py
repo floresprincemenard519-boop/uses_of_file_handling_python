dcea26a4c631211c89ca7df0ed08cefd4e12a529
Prince Menard T. Flores <floresprincemenard519@gmail.com>
2026-04-27 10:11:20 +0800

    Added try catch and found some errors that have to be handled more.

--- how_pythons_read/student_grade_reader.py
+++ how_pythons_read/student_grade_reader.py
@@ -1,12 +1,19 @@
 highest_grade = 5
-top_student = ""
+top_student = "No student in file."
 
-with open("student_grades.txt", "r") as file:
-    for line in file:
-        student_name, grade = line.strip().split(",")
-        grade = float(grade)
-        if grade < highest_grade:
-            highest_grade = grade
-            top_student = student_name
+try:
+    with open("student_grades.txt", "r") as file:
+        for line in file:
+            student_name, grade = line.strip().split(",")
+            try:
+                grade = float(grade)
+            except ValueError:
+                print(f"Error: Invalid grade for student {student_name}.")
+                continue
+            if grade < highest_grade:
+                highest_grade = grade
+                top_student = student_name
+except FileNotFoundError:
+    print("Error: File not found.")
 
 print(f"Top Student: {top_student} with a grade of {highest_grade}")
